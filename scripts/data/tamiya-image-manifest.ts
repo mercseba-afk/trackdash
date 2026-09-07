59a69c5ebeae34daf154163b2df0c279143c1a9a
mercseba-afk <merc.seba@gmail.com>
2026-09-07 22:11:40 +0200

    chore: include catalog foundation image batch

--- scripts/data/tamiya-image-manifest.ts
+++ scripts/data/tamiya-image-manifest.ts
@@ -3,6 +3,7 @@ import { TAMIYA_IMAGES_BATCH2 } from "./tamiya-images-batch2"
 import { TAMIYA_IMAGES_BATCH3 } from "./tamiya-images-batch3"
 import { TAMIYA_IMAGES_BATCH4 } from "./tamiya-images-batch4"
 import { TAMIYA_IMAGES_BATCH5 } from "./tamiya-images-batch5"
+import { TAMIYA_IMAGES_BATCH6 } from "./tamiya-images-batch6"
 
 // Canonical aggregate consumed by the image validator and SQL generator.
 // Keeping audit batches in small append-only data files makes future image
@@ -13,4 +14,5 @@ export const TAMIYA_IMAGES = [
   ...TAMIYA_IMAGES_BATCH3,
   ...TAMIYA_IMAGES_BATCH4,
   ...TAMIYA_IMAGES_BATCH5,
+  ...TAMIYA_IMAGES_BATCH6,
 ]
