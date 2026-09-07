16bc47c057bc847a5203c95b552ff55f065bfc8f
mercseba-afk <merc.seba@gmail.com>
2026-09-07 18:52:57 +0200

    chore: include no-photo image batch in manifest

--- scripts/data/tamiya-image-manifest.ts
+++ scripts/data/tamiya-image-manifest.ts
@@ -1,6 +1,7 @@
 import { TAMIYA_IMAGES as TAMIYA_IMAGES_BASE } from "./tamiya-images"
 import { TAMIYA_IMAGES_BATCH2 } from "./tamiya-images-batch2"
 import { TAMIYA_IMAGES_BATCH3 } from "./tamiya-images-batch3"
+import { TAMIYA_IMAGES_BATCH4 } from "./tamiya-images-batch4"
 
 // Canonical aggregate consumed by the image validator and SQL generator.
 // Keeping audit batches in small append-only data files makes future image
@@ -9,4 +10,5 @@ export const TAMIYA_IMAGES = [
   ...TAMIYA_IMAGES_BASE,
   ...TAMIYA_IMAGES_BATCH2,
   ...TAMIYA_IMAGES_BATCH3,
+  ...TAMIYA_IMAGES_BATCH4,
 ]
