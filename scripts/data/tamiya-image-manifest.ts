9436b1ab93a54f1857c5902e9368816deefa6077
mercseba-afk <merc.seba@gmail.com>
2026-09-07 19:20:19 +0200

    chore: include hierarchy audit images

--- scripts/data/tamiya-image-manifest.ts
+++ scripts/data/tamiya-image-manifest.ts
@@ -2,6 +2,7 @@ import { TAMIYA_IMAGES as TAMIYA_IMAGES_BASE } from "./tamiya-images"
 import { TAMIYA_IMAGES_BATCH2 } from "./tamiya-images-batch2"
 import { TAMIYA_IMAGES_BATCH3 } from "./tamiya-images-batch3"
 import { TAMIYA_IMAGES_BATCH4 } from "./tamiya-images-batch4"
+import { TAMIYA_IMAGES_BATCH5 } from "./tamiya-images-batch5"
 
 // Canonical aggregate consumed by the image validator and SQL generator.
 // Keeping audit batches in small append-only data files makes future image
@@ -11,4 +12,5 @@ export const TAMIYA_IMAGES = [
   ...TAMIYA_IMAGES_BATCH2,
   ...TAMIYA_IMAGES_BATCH3,
   ...TAMIYA_IMAGES_BATCH4,
+  ...TAMIYA_IMAGES_BATCH5,
 ]
