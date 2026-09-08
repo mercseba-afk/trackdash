1250efc396a9f28cc953fa96efba1386bb7f6a6a
mercseba-afk <merc.seba@gmail.com>
2026-09-08 09:29:22 +0200

    feat: add messages route

---
+++ app/messages/page.tsx
@@ -0,0 +1,10 @@
+import { AppPage } from "@/components/app-page"
+import { MessagesScreen } from "@/components/screens/messages-screen"
+
+export default function MessagesPage() {
+  return (
+    <AppPage>
+      <MessagesScreen />
+    </AppPage>
+  )
+}
