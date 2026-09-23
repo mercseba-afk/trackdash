86fecb41913d4341354a0a85066fd87d973df0ec
mercseba-afk <merc.seba@gmail.com>
2026-09-23 13:01:04 +0200

    Pin canonical vertical reference ids

--- lib/verticals.ts
+++ lib/verticals.ts
@@ -13,11 +13,15 @@ export const COLLECTIBLE_VERTICALS = {
     slug: "mini4wd",
     label: "Mini 4WD",
     brandSlug: "tamiya",
+    brandId: "382feca9-48e9-5144-a92d-41f77fb7e438",
+    categoryId: "cd755fcb-2bc5-5975-8ec0-f45e7df891cc",
   },
   hotwheels: {
     slug: "hotwheels",
     label: "Hot Wheels",
     brandSlug: "mattel",
+    brandId: "6f100164-74bd-56bd-9dd1-221ea269ed8a",
+    categoryId: "cdaaff01-f4f9-52ff-951a-ddbbba542d5c",
   },
 } as const
 
